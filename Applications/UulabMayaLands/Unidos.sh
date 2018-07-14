1492df2a9f2d4610372c84e080fe53fa8e495cd4
Abraham Arce <xe1gyq@gmail.com>
2018-07-13 22:18:41 -0500

    Create Unidos.sh

---
+++ Applications/UulabMayaLands/Unidos.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export UNIDOS_PID=$$
+export UNIDOS_NAME="Unidos"
+export UNIDOS_PATH=$(readlink -f "$0")
+export UNIDOS_DIRECTORY=$(dirname "$UNIDOS_PATH")
+
+# Unidos
+LOCAL_INSTITUTION_NAME=`Unidos`
+LOCAL_EVENT_NAME_SPANISH="Visital Intel 2018"
+LOCAL_PHRASE="Hola \
+              Mundo"
+
+LOCAL_LANGUAGE[0]="spanish"
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+chooseLanguage()
+{
+    rand=$[ $RANDOM % 2 ]
+    LOCAL_LANGUAGE=${LOCAL_LANGUAGE[$rand]}
+}
+
+# =============================================================================
+# Main
+# =============================================================================
+
+LogPid.sh $UNIDOS_PID $UNIDOS_NAME
+
+
+
+# End of File
