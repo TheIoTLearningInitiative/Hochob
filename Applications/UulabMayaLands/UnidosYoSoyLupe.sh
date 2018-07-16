54561ffebdb24033e2d45ebc8ddad119332d165e
Abraham Arce <xe1gyq@gmail.com>
2018-07-16 07:24:06 -0500

    Update UnidosYoSoyLupe.sh

--- Applications/UulabMayaLands/UnidosYoSoyLupe.sh
+++ Applications/UulabMayaLands/UnidosYoSoyLupe.sh
@@ -20,11 +20,11 @@ LOCAL_PHRASE="Bienvenidos Unidos a mi casa, su casa. \
      Mis pasatiempos favoritos son compartir tiempo con mi familia, jugar al aire libre, \
      leer cuentos divertidos, hacer ejercicio y aprender con mis familiares y amigos \
      todo el conocimiento científico y tecnológico que está presente en nuestra vida cotidiana. \
-     También soy guia en el emocionante Centro de Visitantes de Intel. \
+     También soy guía en el emocionante Centro de Visitantes de Intel. \
      Hace rato estaba un poco chiviado pero ya estoy listo para mostrarles \
      lo interesante que es el mundo de las Ciencias, la Tecnología, \
-     las Ingenierías y las Matemáticas. A nombre de cada uno de los Ingenieros \
-     que aquí trabajamos les deseamos la mejor de las experiencias. \
+     las Ingenierías y las Matemáticas. A nombre de cada uno de los de los grandes \
+     seres humanos que aquí trabajan les deseamos la mejor de las experiencias. \
      Muchas gracias por haber venido. \
     "
 
