bb553df4c2ccc192308e93f48eb7759e6a873e45
Abraham Arce <xe1gyq@gmail.com>
2018-07-16 07:34:43 -0500

    Update UnidosActividades.sh

--- Applications/UulabMayaLands/UnidosActividades.sh
+++ Applications/UulabMayaLands/UnidosActividades.sh
@@ -15,12 +15,13 @@ export UNIDOS_DIRECTORY=$(dirname "$UNIDOS_PATH")
 
 # Unidos Actividades
 LOCAL_PHRASE="Primero, escucharemos a un Ingeniero guapo e inteligente platicarnos \
-              sobre la importancia de estudiar Ingenieria. \
-              Despues pasaremos a nuestras mesas de trabajo donde personas \
+              sobre la importancia de estudiar Ingeniería. \
+              Después pasaremos a nuestras mesas de trabajo donde personas \
               lindas y hermosas tanto de Unidos como de Intel te ayudaran a ensamblar \ 
-              tu primera computadora, armaras y transformaras tu primer carro autonomo, 
-              jugaras con distintos tipos de robots y para terminar con nuestra diversion, \
-              jugaremos loteria. Deseo de todo corazon que todos Unidos se diviertan.\
+              tu primera computadora, armaras y transformaras tu primer carro autónomo, 
+              jugaras con distintos tipos de robots y para complementar con nuestra diversión, \
+              jugaremos lotería, yo el Robot Lupe, diciéndoles las cartas. \
+              Deseo de todo corazón que todos Unidos se diviertan.\
               "
 
 # =============================================================================
