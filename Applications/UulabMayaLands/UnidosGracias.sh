d2f7a7ab76ad0eb07ed0812c609e21c461e3956e
Abraham Arce <xe1gyq@gmail.com>
2018-07-16 07:57:29 -0500

    Update UnidosGracias.sh

--- Applications/UulabMayaLands/UnidosGracias.sh
+++ Applications/UulabMayaLands/UnidosGracias.sh
@@ -16,14 +16,17 @@ export UNIDOS_DIRECTORY=$(dirname "$UNIDOS_PATH")
 # Unidos Buenos Dias
 LOCAL_PHRASE="Todos tenemos un lugar en el mundo, una misión en este hermoso camino que llamamos vida, \
               y cada vez que nos movemos, lo estremecemos y lo hacemos más grande. \
-              Gracias Amigos por ser nuestra fuente de inspiración para buscar formar una sociedad incluyente \
+              Gracias Amigos por ser nuestra fuente de fuente de inspiración en la búsqueda de alternativas \
+              para construir una sociedad incluyente \
               y sensible que una nuestros corazones y a través de experiencias generemos juntos empatía. \
               Gracias Aliados por permitirse desarrollar otras habilidades, alcanzar la plenitud, \
               experimentar otras realidades y tener una transformación personal. \
-              Gracias Unidos por unirnos como personas con y sin discapacidad para complementarnos \
+              Gracias Unidos por la oportunidad de estrechar lazos de amistad entre las distintas personas que \
+              vienen aquí a convivir y complementarse, siendo posible \
               y visualizar lo que podemos lograr si avanzamos Unidos, y así, transformamos juntas nuestras vidas. \
               Estamos hoy todos reunidos para devolver la conciencia al mundo. \
-              Dispongamos a Compartir, Confiar y transformar. Es un honor tenerlos aquí con nosotros.\
+              Gracias por Compartir, Confiar y transformar. Es un honor tenerlos aquí con nosotros.\
+              Intel dice gracias.\
               "
 
 # =============================================================================
