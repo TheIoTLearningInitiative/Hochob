8fe83b7042767a9bba24b07d5576e5f142c0c6e4
Abraham Arce <abraham.arce.moreno@intel.com>
2018-11-29 05:10:16 -0600

    Update
    Signed-off-by: Abraham Arce <abraham.arce.moreno@intel.com>

--- Applications/GameMayaLands/LoteriaMexicana/LoteriaMexicana.py
+++ Applications/GameMayaLands/LoteriaMexicana/LoteriaMexicana.py
@@ -28,7 +28,7 @@ with open("LasCartasSimple.rnd", "w") as f:
         os.system(voice_command)
         #os.system('mosquitto_pub -h 10.215.56.158 -p 1883 -q 2 -t amikoo/tradition/image -m LoteriaMexicana' + '/' + image)
         #os.system('mosquitto_pub -h 10.215.56.158 -p 1883 -q 2 -t amikoo/sound/speechsynthetizer/decir -m "' + line + '"')
-        time.sleep(5)
+        time.sleep(10)
     f.close()
 
 #os.system('espeak -v es-la -a 100 -p 50 -s 170 -f LasCartasSimple.rnd -w LasCartasSimple.wav')
