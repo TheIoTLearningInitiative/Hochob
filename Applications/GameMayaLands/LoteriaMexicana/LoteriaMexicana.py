45f2857a27d6c1d7e54b02ffc81254bc3cfc0de0
Abraham Arce <xe1gyq@gmail.com>
2018-11-29 20:09:50 -0600

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Applications/GameMayaLands/LoteriaMexicana/LoteriaMexicana.py
+++ Applications/GameMayaLands/LoteriaMexicana/LoteriaMexicana.py
@@ -24,7 +24,7 @@ with open("LasCartasSimple.rnd", "w") as f:
         image_path = 'LoteriaMexicana/Image/' + image
         image_command = 'eog -f ' + image_path + ' &'
         os.system(image_command)
-        voice_command = 'espeak -v es-la -a 100 -p 50 -s 170 ' + line
+        voice_command = 'espeak -v es-la -a 100 -p 50 -s 170 \"%s\"' % line
         os.system(voice_command)
         #os.system('mosquitto_pub -h 10.215.56.158 -p 1883 -q 2 -t amikoo/tradition/image -m LoteriaMexicana' + '/' + image)
         #os.system('mosquitto_pub -h 10.215.56.158 -p 1883 -q 2 -t amikoo/sound/speechsynthetizer/decir -m "' + line + '"')
